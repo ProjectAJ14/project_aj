41cf5da5112512a841d16a20da3c18cce6c33eb6
Ajay Kumar <ajay.kumar@nonstopio.com>
2022-09-19 21:34:32 +0530

    Updated titles/text and theme

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Project_AJ'),
     );
   }
 }
@@ -96,7 +96,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Flutter Web hosted on GitHub Pages',
+              style: TextStyle(fontSize: 50),
             ),
             Text(
               '$_counter',
@@ -107,8 +108,8 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'thumb_up',
+        child: const Icon(Icons.thumb_up),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
